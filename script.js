0451c4547f328f3935f49b18758a48c2785c3137
Tanaphol Fowler <tanfow99@gmail.com>
2023-06-16 05:49:42 -0700

    refactor: delete parts that include home The 'Home' part is being scrapped so everything relating to it in each file is being deleted.

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 const sidebar_btn = document.querySelector('.hamburger');
-const home_btn = document.getElementById('home');
 const todo_btn = document.getElementById('todo');
 const project_btn = document.getElementById('project');
 const notes_btn = document.getElementById('notes');
@@ -8,8 +7,6 @@ const popup_todo_btn = document.getElementById('popup_todo');
 const popup_project_btn = document.getElementById('popup_project');
 const popup_note_btn = document.getElementById('popup_note');
 
-
-const homeIsActive = 'home';
 const todoIsActive = 'todo';
 const projectIsActive = 'project';
 const notesIsActive = 'notes';
@@ -26,28 +23,19 @@ function toggleSidebar() {
 }
 
 function toggleMainContent(isActive) {
-    let home_contents = document.querySelector(".home-contents");
     let todo_contents = document.querySelector(".todo-contents");
     let project_contents = document.querySelector(".project-contents");
     let notes_contents = document.querySelector(".notes-contents");
-
-    if(isActive == homeIsActive){
-        home_contents.style.display = "flex";
-        todo_contents.style.display = "none";
-        project_contents.style.display = "none";
-        notes_contents.style.display = "none";
-    } else if(isActive == todoIsActive) {
-        home_contents.style.display = "none";
+    
+    if(isActive == todoIsActive) {
         todo_contents.style.display = "flex";
         project_contents.style.display = "none";
         notes_contents.style.display = "none";
     } else if(isActive == projectIsActive) {
-        home_contents.style.display = "none";
         todo_contents.style.display = "none";
         project_contents.style.display = "flex";
         notes_contents.style.display = "none";
     } else if(isActive == notesIsActive) {
-        home_contents.style.display = "none";
         todo_contents.style.display = "none";
         project_contents.style.display = "none";
         notes_contents.style.display = "flex";
@@ -76,9 +64,6 @@ function togglePopupContent(isActive){
         popup_notes_contents.style.display = "block";
     } 
 }
-home_btn.addEventListener('click', function(){
-    toggleMainContent(homeIsActive);
-});
 
 todo_btn.addEventListener('click', function(){
     toggleMainContent(todoIsActive);
