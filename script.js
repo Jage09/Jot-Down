b3e5b3792bd54c3dada36f98381b1b1510666a95
Tanaphol Fowler <tanfow99@gmail.com>
2023-06-16 03:01:54 -0700

    feat: add ability to toggle which popup to show up Each item in the popup menu is now clickable and changes the main content within the popup corresponding to the type of item.

--- script.js
+++ script.js
@@ -3,11 +3,19 @@ const home_btn = document.getElementById('home');
 const todo_btn = document.getElementById('todo');
 const project_btn = document.getElementById('project');
 const notes_btn = document.getElementById('notes');
-const popup_btn = document.getElementById('popup')
-var homeIsActive = 'home';
-var todoIsActive = 'todo';
-var projectIsActive = 'project';
-var notesIsActive = 'notes';
+const popup_btn = document.getElementById('popup');
+const popup_todo_btn = document.getElementById('popup_todo');
+const popup_project_btn = document.getElementById('popup_project');
+const popup_note_btn = document.getElementById('popup_note');
+
+
+const homeIsActive = 'home';
+const todoIsActive = 'todo';
+const projectIsActive = 'project';
+const notesIsActive = 'notes';
+const popupTodoIsActive = 'popup_todo';
+const popupProjectIsActive = 'popup_project';
+const popupNotesIsActive = 'popup_note';
 // menu_btn.addEventListener('click', function (){
 //     menu_btn.classList.toggle('is-active');
 // });
@@ -49,6 +57,25 @@ function togglePopup() {
     popup_btn.classList.toggle('active');
 }
 
+function togglePopupContent(isActive){
+    let popup_todo_contents = document.querySelector(".popup-todo-contents");
+    let popup_project_contents = document.querySelector(".popup-project-contents");
+    let popup_notes_contents = document.querySelector(".popup-notes-contents");
+
+    if(isActive == popupTodoIsActive){
+        popup_todo_contents.style.display = "flex";
+        popup_project_contents.style.display = "none";
+        popup_notes_contents.style.display = "none";
+    } else if(isActive == popupProjectIsActive) {
+        popup_todo_contents.style.display = "none";
+        popup_project_contents.style.display = "flex";
+        popup_notes_contents.style.display = "none";
+    } else if(isActive == popupNotesIsActive) {
+        popup_todo_contents.style.display = "none";
+        popup_project_contents.style.display = "none";
+        popup_notes_contents.style.display = "flex";
+    } 
+}
 home_btn.addEventListener('click', function(){
     toggleMainContent(homeIsActive);
 });
@@ -63,4 +90,16 @@ project_btn.addEventListener('click', function(){
 
 notes_btn.addEventListener('click', function(){
     toggleMainContent(notesIsActive);
+});
+
+popup_todo_btn.addEventListener('click', function(){
+    togglePopupContent(popupTodoIsActive);
+});
+
+popup_project_btn.addEventListener('click', function(){
+    togglePopupContent(popupProjectIsActive);
+});
+
+popup_note_btn.addEventListener('click', function(){
+    togglePopupContent(popupNotesIsActive);
 });
